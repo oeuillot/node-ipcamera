cfd9495ce0d1f9a1c4fdd575b6d20d4a6ffa5321
oeuillot <olivier.oeuillot@gmail.com>
2014-08-05 12:37:47 +0200

    Try fault tolerance

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var program = require('commander');
+var program = require('commanEventEmitter. der');
 var url = require('url');
 var fs = require('fs');
 var http = require('http');
 var child = require('child_process');
 var express = require('express');
+var Events = require('events');
 
 var IPCamStream = require('./lib/ipCamStream');
 var MpegStream = require('./lib/mpegStream');
@@ -26,61 +27,7 @@ if (!program.ffmpeg) {
 
 program.ffmpegArgs = program.ffmpegArgs || "-an -f m4v -i - -r 5 -qmin 1 -q:v 2 -s 720x576 -f mjpeg -";
 
-var videoURL = url.parse(program.url);
-videoURL.headers = {
-	accept: '*/*'
-};
-
-var lastMjpegStream;
-
-var request = http.request(videoURL, function(response) {
-	// console.log('STATUS: ', response.statusCode);
-	// console.log('HEADERS: ', response.headers);
-
-	if (response.statusCode != 200) {
-		throw new Error("Invalid status code of response " + response.statusCode);
-	}
-
-	var mpegStream = new MpegStream();
-	if (false) {
-		mpegStream.on('frame', function(frame) {
-			console.log("Get frame ", frame);
-		});
-	}
-
-	var mjpegStream = new MjpegStream();
-	lastMjpegStream = mjpegStream;
-
-	var ipCamStream = new IPCamStream();
-
-
-	var readable = response.pipe(ipCamStream).pipe(mpegStream);
-
-	var ffmpeg = child.spawn(program.ffmpeg, program.ffmpegArgs.split(" "));
-
-	var cnt1 = 0;
-	var cnt2 = 0;
-
-	// readable.pipe(ffmpeg.stdin);
-	readable.on("data", function(data) {
-		// console.log("Send mpeg " + (cnt1++));
-		ffmpeg.stdin.write(data);
-	});
-
-	ffmpeg.stdout.on('data', function(data) {
-		// console.log("Get jpeg " + (cnt2++));
-
-		mjpegStream.write(data);
-	});
-
-	ffmpeg.stderr.pipe(process.stderr);
-});
-
-request.on('error', function(e) {
-	console.log('problem with request: ' + e.message);
-});
-
-request.end();
+var lastJpegEventEmitter = new Events.EventEmitter();
 
 var mimeBoudary = "--YOYOVAENVACANCES--";
 
@@ -94,19 +41,19 @@ app.get("/mjpeg", function(req, res) {
 		'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
 	});
 
-	lastMjpegStream.once("jpeg", function sendJpeg(jpeg) {
+	lastJpegEventEmitter.once("jpeg", function sendJpeg(jpeg) {
 
 		res.write(mimeBoudary + '\r\nContent-Type: image/jpeg\r\nContent-Length: ' + jpeg.size + '\r\n\r\n');
 		res.write(jpeg.data);
 		res.write('\r\n');
 
-		lastMjpegStream.once("jpeg", sendJpeg);
+		lastJpegEventEmitter.once("jpeg", sendJpeg);
 	});
 });
 
 app.get("/jpeg", function(req, res) {
 
-	lastMjpegStream.once("jpeg", function sendJpeg(jpeg) {
+	lastJpegEventEmitter.once("jpeg", function sendJpeg(jpeg) {
 
 		res.writeHead(200, {
 			'Content-Type': 'image/jpeg',
@@ -120,3 +67,62 @@ app.get("/jpeg", function(req, res) {
 	});
 });
 app.listen(program.port || 8080);
+
+newRequest();
+
+function newRequest() {
+
+	var videoURL = url.parse(program.url);
+	videoURL.headers = {
+		accept: '*/*'
+	};
+
+	var request = http.request(videoURL, function(response) {
+		// console.log('STATUS: ', response.statusCode);
+		// console.log('HEADERS: ', response.headers);
+
+		if (response.statusCode != 200) {
+			throw new Error("Invalid status code of response " + response.statusCode);
+		}
+
+		var mpegStream = new MpegStream();
+
+		var mjpegStream = new MjpegStream();
+		mjpegStream.on('jpeg', function(jpeg) {
+			lastJpegEventEmitter.emit('jpeg', jpeg);
+		});
+
+		var ipCamStream = new IPCamStream();
+
+		var readable = response.pipe(ipCamStream).pipe(mpegStream);
+
+		var ffmpeg = child.spawn(program.ffmpeg, program.ffmpegArgs.split(" "));
+
+		var cnt1 = 0;
+		var cnt2 = 0;
+
+		// readable.pipe(ffmpeg.stdin);
+		readable.on("data", function(data) {
+			// console.log("Send mpeg " + (cnt1++));
+			ffmpeg.stdin.write(data);
+		});
+
+		ffmpeg.stdout.on('data', function(data) {
+			// console.log("Get jpeg " + (cnt2++));
+
+			mjpegStream.write(data);
+		});
+
+		ffmpeg.stderr.pipe(process.stderr);
+	});
+
+	request.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+
+		newRequest();
+	});
+
+	request.end();
+
+	return request;
+}
