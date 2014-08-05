ac7a8e4c263f97bedd5e86d87517c15b86494e1c
oeuillot <olivier.oeuillot@gmail.com>
2014-08-05 11:18:48 +0200

    First version

---
+++ node-ipcamera/lib/ipCamStream.js
@@ -0,0 +1,190 @@
+var Events = require('events');
+var Util = require('util');
+var BuffersStream = require('./buffersStream');
+
+var logVOL = false;
+var logVOP = false;
+
+var VideoStream = function() {
+	BuffersStream.call(this);
+
+	var self = this;
+
+	this.once('vol', function(vol) {
+		// console.log("Get VOL ", vol);
+
+		setImmediate(self.decodeVOPHeader.bind(self));
+	});
+
+	this.on('vop', function(vop) {
+		// console.log("Get VOP ", vop);
+
+		setImmediate(self.decodeVOPHeader.bind(self));
+	});
+
+	this.decodeVOLHeader();
+}
+
+Util.inherits(VideoStream, BuffersStream);
+module.exports = VideoStream;
+
+var proto = {
+	decodeVOLHeader: function() {
+		if (logVOL) {
+			console.log("Wait VOL");
+		}
+
+		var buffer = this.read(16);
+
+		if (logVOL) {
+			console.log("Wait VOL => ", buffer);
+		}
+		if (!buffer) {
+			this.once('bufferReady', this.decodeVOLHeader.bind(this));
+			return;
+		}
+
+		if (buffer[0] != 0x00 || buffer[1] != 0x00 || buffer[2] != 0x01 || buffer[3] != 0xa5) {
+			var newBuffer = buffer.slice(1);
+			this.unshift(newBuffer);
+
+			if (true) {
+				console.log("Unshift VOL => ", newBuffer);
+			}
+			setImmediate(this.decodeVOLHeader.bind(this));
+			return;
+		}
+
+		var header = {
+			dataSize: buffer[5] | (buffer[6] << 8) | (buffer[7] << 16),
+			controlData: (buffer[8] & 0x1) > 0,
+
+			singleIFrameIndication: (buffer[8] & 0x2) > 0,
+			displayResolution: buffer[9] | ((buffer[10] & 0x80) << 1),
+
+			audioFormat: buffer[11] & 0x01,
+			audioCompression: (buffer[11] & 0x06) >> 1
+		};
+
+		if (logVOL) {
+			console.log("Get VOL => ", header);
+		}
+
+		this.emit('volHeader', header);
+
+		this.decodeVOLData(header);
+	},
+
+	decodeVOLData: function(volHeader) {
+
+		if (logVOL) {
+			console.log("Wait VOL.Data");
+		}
+
+		var volData = this.read(volHeader.dataSize);
+
+		if (logVOL) {
+			console.log("Wait VOL.Data => ", volData);
+		}
+
+		if (!volData) {
+			this.once('bufferReady', this.decodeVOLData.bind(this, volHeader));
+			return;
+		}
+
+		this.emit('volData', volData);
+
+		volHeader.data = volData;
+		volHeader.timestamp = Date.now();
+
+		if (logVOL) {
+			console.log("Wait VOL completed ", volHeader);
+		}
+
+		this.emit('vol', volHeader);
+
+		this.emit('data', volData);
+	},
+	decodeVOPHeader: function() {
+		if (logVOP) {
+			console.log("Wait VOP");
+		}
+
+		var buffer = this.read(16);
+
+		if (logVOP) {
+			console.log("Wait VOP => ", buffer);
+		}
+
+		if (!buffer) {
+			this.once('bufferReady', this.decodeVOPHeader.bind(this));
+			return;
+		}
+
+		if (buffer[0] != 0x00 || buffer[1] != 0x00 || buffer[2] != 0x01 || buffer[3] != 0xa5) {
+			var newBuffer = buffer.slice(1);
+			this.unshift(newBuffer);
+			setImmediate(this.decodeVOPHeader.bind(this));
+
+			if (true) {
+				console.log("Unshift VOP => ", newBuffer);
+			}
+			return;
+		}
+
+		var header = {
+			dataDefinition: (buffer[4] & 0x1),
+			videoAudioIndicator: (buffer[4] & 0x6) >> 1,
+			wayAudioIndicator: (buffer[4] & 0x18) >> 3,
+			dataSize: buffer[5] | (buffer[6] << 8) | (buffer[7] << 16),
+
+			controlDataIndicator: (buffer[8] & 0x1),
+			frameRate: (buffer[8] & 0x76) >> 2,
+			frameType: (buffer[9] & 0x20) >> 6,
+			startOfFrame: (buffer[9] & 0x40) > 0,
+			endOfFrame: (buffer[9] & 0x80) > 0
+		};
+
+		this.emit('vopHeader', header);
+
+		if (logVOP) {
+			console.log("Wait VOP header => ", header);
+		}
+
+		this.decodeVOPData(header);
+	},
+
+	decodeVOPData: function(vopHeader) {
+		if (logVOP) {
+			console.log("Wait VOP.Data");
+		}
+
+		var vopData = this.read(vopHeader.dataSize);
+
+		if (logVOP) {
+			console.log("Wait VOP.Data => ", vopData);
+		}
+
+		if (!vopData) {
+			this.once('bufferReady', this.decodeVOPData.bind(this, vopHeader));
+			return;
+		}
+
+		this.emit('vopData', vopData);
+
+		vopHeader.data = vopData;
+		vopHeader.timestamp = Date.now();
+
+		if (logVOP) {
+			console.log("Wait VOP.Data completed ", vopData);
+		}
+
+		this.emit('vop', vopHeader);
+
+		this.emit('data', vopData);
+	}
+};
+
+for ( var i in proto) {
+	VideoStream.prototype[i] = proto[i];
+}
---
+++ node-ipcamera/lib/mjpegStream.js
@@ -0,0 +1,114 @@
+var Events = require('events');
+var Util = require('util');
+var fs = require('fs');
+var BuffersStream = require('./buffersStream');
+
+var logPositions = false;
+
+var MJPEGStream = function() {
+	BuffersStream.call(this);
+
+	var self = this;
+
+	this.on('jpeg', function(vop) {
+		setImmediate(self.readNextJpeg.bind(self));
+	});
+
+	this.readNextJpeg();
+}
+
+Util.inherits(MJPEGStream, BuffersStream);
+module.exports = MJPEGStream;
+
+var cnt = 0;
+
+var proto = {
+	readNextJpeg: function(start, lastSearch) {
+		if (start === undefined || start < 0) {
+			start = this.searchMark(0, 0xFF, 0xD8);
+			if (start < 0) {
+				this.once('bufferReady', this.readNextJpeg.bind(this));
+				return null;
+			}
+		}
+
+		if (logPositions) {
+			console.log("Start=", start);
+		}
+
+		if (start) {
+			if (logPositions) {
+				console.log("***** SKIP", start);
+			}
+
+			this.skip(start);
+		}
+
+		if (!lastSearch || lastSearch < 4) {
+			lastSearch = 4;
+		}
+
+		var end = this.searchMark(lastSearch, 0xFF, 0xD9);
+		if (logPositions) {
+			console.log("End=", end);
+		}
+
+		if (end < 0) {
+			this.once('bufferReady', this.readNextJpeg.bind(this, 0, this.bufferSize - 1));
+			return null;
+		}
+		end += 2;
+
+		var buffer = this.read(end);
+
+		if (logPositions) {
+			console.log("Make buffer to ", end, " => ", buffer);
+		}
+
+		var frame = {
+			data: buffer,
+			size: buffer.length,
+			timestamp: Date.now()
+		}
+
+		this.emit('jpeg', frame);
+
+		this.emit('data', buffer);
+
+		if (false) {
+			var writable = fs.createWriteStream('c:/temp/j' + (cnt++) + '.jpg');
+			writable.write(buffer);
+			writable.close();
+		}
+
+		return frame;
+	},
+
+	searchMark: function(position, firstValue, secondValue) {
+		for (;; position += 2) {
+			var p = this.peek(position);
+			if (p < 0) {
+				return -1;
+			}
+
+			if (p == firstValue) {
+				if (this.peek(position + 1) == secondValue) {
+					return position;
+				}
+				continue;
+			}
+
+			if (p == secondValue) {
+				if (this.peek(position - 1) == firstValue) {
+					return position - 1;
+				}
+				continue;
+			}
+		}
+	}
+
+};
+
+for ( var i in proto) {
+	MJPEGStream.prototype[i] = proto[i];
+}
