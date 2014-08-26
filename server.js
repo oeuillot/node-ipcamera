ecf5c6be4c5da00f83675dd06596092bb61999a7
oeuillot <olivier.oeuillot@gmail.com>
2014-08-26 22:29:57 +0200

    Add store engine

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var http = require('http');
 var child = require('child_process');
 var express = require('express');
 var Events = require('events');
+var path = require('path');
 
 var NO_CACHE_CONTROL = "no-cache, private, no-store, must-revalidate, max-stale=0, max-age=1,post-check=0, pre-check=0";
 
@@ -20,6 +21,9 @@ program.option("-r, --outputRate <rate>", "FFMpeg video output rate");
 program.option("--inputRate <rate>", "FFMpeg video input rate");
 program.option("--localtime", "Add localtime on frame");
 program.option("--fontPath <fontPath>", "Font path used by localtime");
+program.option("--storePath <storePath>", "Path where to store images");
+program.option("--storeTimeout <minutes>", "Delay after which the images were deleted");
+program.option("--storeFPS <storeFPS>", "Stored frames per second", parseInt);
 
 program.parse(process.argv);
 
@@ -50,7 +54,7 @@ ffmpegArgs.push("-")
 
 var lastJpegEventEmitter = new Events.EventEmitter();
 
-var mimeBoudary = "--YOYOVAENVACANCES--";
+var mimeBoudary = "--OLIVIERVAENVACANCES--";
 
 var app = express();
 
@@ -59,13 +63,19 @@ app.get("/mjpeg", function(req, res) {
 	res.writeHead(200, {
 		'Content-Type': 'multipart/x-mixed-replace; boundary="' + mimeBoudary + '"',
 		'Transfer-Encoding': 'chunked',
-		'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+		'Cache-Control': NO_CACHE_CONTROL
 	});
 
 	lastJpegEventEmitter.once("jpeg", function sendJpeg(jpeg) {
 
-		res.write(mimeBoudary + '\r\nContent-Type: image/jpeg\r\nContent-Length: ' + jpeg.size + '\r\nCache-Control: ' +
-				NO_CACHE_CONTROL + '\r\n\r\n');
+		var headers = 'Content-Type: image/jpeg\r\nContent-Length: ' + jpeg.size + '\r\nCache-Control: ' +
+				NO_CACHE_CONTROL + '\r\n';
+
+		if (jpeg.timestamp) {
+			headers += 'X-Image-Date: ' + (new Date(jpeg.timestamp)).toISOString() + '\r\n';
+		}
+
+		res.write(mimeBoudary + '\r\n' + headers + '\r\n');
 		res.write(jpeg.data);
 		res.write('\r\n');
 
@@ -81,12 +91,18 @@ app.get("/jpeg", function(req, res) {
 
 	lastJpegEventEmitter.once("jpeg", function sendJpeg(jpeg) {
 
-		res.writeHead(200, {
+		var headers = {
 			'Content-Type': 'image/jpeg',
 			'Content-Length': jpeg.size,
 			'Transfer-Encoding': 'chunked',
-			'Cache-Control': NO_CACHE_CONTROL
-		});
+			'Cache-Control': NO_CACHE_CONTROL,
+		};
+
+		if (jpeg.timestamp) {
+			headers['X-Image-Date'] = (new Date(jpeg.timestamp)).toISOString();
+		}
+
+		res.writeHead(200, headers);
 
 		res.write(jpeg.data);
 		res.end();
@@ -213,4 +229,155 @@ function newRequest() {
 	request.end();
 
 	return request;
+}
+
+if (program.storePath) {
+
+	var fdKey = 0;
+	var secondKey = 0;
+	var nextMs = 1000 / (program.storeFPS || 10);
+	var indexBySecond = 0;
+	var firstImageDate;
+
+	var bufInit = new Buffer('Content-Type: multipart/x-mixed-replace; boundary="' + mimeBoudary + '"\r\n\r\n');
+	var bufEnd = new Buffer('\r\n');
+
+	lastJpegEventEmitter.once("jpeg", function writeJpeg(jpeg) {
+
+		function writeBuffer() {
+			var headers = 'Content-Type: image/jpeg\r\nContent-Length: ' + jpeg.size + '\r\nCache-Control: ' +
+					NO_CACHE_CONTROL + '\r\n';
+
+			if (jpeg.timestamp) {
+				headers += 'X-Image-Date: ' + (new Date(jpeg.timestamp)).toISOString() + '\r\n';
+			}
+
+			var buf = new Buffer(mimeBoudary + '\r\n' + headers + '\r\n');
+
+			fs.write(fdKey, buf, 0, buf.length, null, function(error) {
+				if (error) {
+					console.error("Can not write head of file '" + p + "': " + error);
+					return;
+				}
+
+				fs.write(fdKey, jpeg.data, 0, jpeg.data.length, null, function(error) {
+					if (error) {
+						console.error("Can not write file '" + p + "': " + error);
+						return;
+					}
+
+					fs.write(fdKey, bufEnd, 0, bufEnd.length, null, function(error) {
+						if (error) {
+							console.error("Can not write head of file '" + p + "': " + error);
+							return;
+						}
+
+						lastJpegEventEmitter.once("jpeg", writeJpeg);
+					});
+				});
+			});
+		}
+
+		var date = new Date(jpeg.timestamp || Date.now());
+
+		var d = new Date(date.getTime());
+		d.setMilliseconds(0);
+
+		if (d.getTime() == secondKey) {
+
+			var diffMs = date.getTime() - firstImageDate.getTime();
+			if (diffMs < indexBySecond * nextMs) {
+				lastJpegEventEmitter.once("jpeg", writeJpeg);
+				return;
+			}
+
+			indexBySecond++;
+
+			writeBuffer();
+			return;
+		}
+
+		secondKey = d.getTime();
+		if (fdKey) {
+			fs.close(fdKey);
+			fdKey = 0;
+		}
+		indexBySecond = 1;
+		firstImageDate = date;
+
+		var py = path.join(program.storePath, String(date.getFullYear()));
+		try {
+			fs.statSync(py);
+		} catch (x) {
+			if (x.code == 'ENOENT') {
+				fs.mkdirSync(py);
+			} else {
+				console.log(x);
+				throw x;
+			}
+		}
+
+		var mn = date.getMonth() + 1;
+		var md = date.getDate();
+		var mh = date.getHours();
+		var mi = date.getMinutes();
+		var ms = date.getSeconds();
+
+		var pm = path.join(py, ((mn < 10) ? "0" : "") + mn);
+		try {
+			fs.statSync(pm);
+		} catch (x) {
+			if (x.code == 'ENOENT') {
+				fs.mkdirSync(pm);
+			} else {
+				console.log(x);
+				throw x;
+			}
+		}
+
+		var pd = path.join(pm, ((md < 10) ? "0" : "") + md);
+		try {
+			fs.statSync(pd);
+		} catch (x) {
+			if (x.code == 'ENOENT') {
+				fs.mkdirSync(pd);
+			} else {
+				console.log(x);
+				throw x;
+			}
+		}
+
+		var ph = path.join(pd, ((mh < 10) ? "0" : "") + mh);
+		try {
+			fs.statSync(ph);
+		} catch (x) {
+			if (x.code == 'ENOENT') {
+				fs.mkdirSync(ph);
+			} else {
+				console.log(x);
+				throw x;
+			}
+		}
+
+		var p = path.join(ph, "Image " + date.getFullYear() + "-" + ((mn < 10) ? "0" : "") + mn + "-" +
+				((md < 10) ? "0" : "") + md + " " + ((mh < 10) ? "0" : "") + mh + "-" + ((mi < 10) ? "0" : "") + mi + "-" +
+				((ms < 10) ? "0" : "") + ms + ".mjpeg");
+
+		fs.open(p, "w", function(error, fd) {
+			if (error) {
+				console.error("Can not create file " + p);
+				return;
+			}
+			fdKey = fd;
+
+			fs.write(fdKey, bufInit, 0, bufInit.length, null, function(error) {
+				if (error) {
+					console.error("Can not write content header : " + error);
+					return;
+				}
+
+				writeBuffer();
+			});
+		});
+	});
 }
