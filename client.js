2fad046c6e414760a75acd10f6865650bd94a664
oeuillot <olivier.oeuillot@gmail.com>
2014-08-28 14:51:26 +0200

    Add --url parameter

--- client.js
+++ client.js
@@ -1,5 +1,14 @@
 var MultipartMjpegDecoderStream = require('./lib/multipartMjpegDecoderStream');
 var http = require('http');
+var program = require('commander');
+
+program.option("-u, --url <url>", "Mjpeg stream URL");
+
+program.parse(process.argv);
+
+if (!program.url) {
+	throw new Error("URL must be specified");
+}
 
 var multipartStream = new MultipartMjpegDecoderStream();
 
@@ -7,7 +16,7 @@ multipartStream.on('jpeg', function(jpeg) {
 	console.error("Receive jpeg: ", jpeg.date.getTime());
 });
 
-var request = http.request("http://delabarre3.oeuillot.net:8089/mjpeg", function(response) {
+var request = http.request(program.url, function(response) {
 
 	if (response.statusCode != 200) {
 		throw new Error("Invalid status code of response " + response.statusCode);
